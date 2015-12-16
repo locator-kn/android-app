a43e1ad05884897bd008a64e0c60ce1cb6fd2751
Dennis Moosmann <dennis.moosmann@gmx.de>
2015-12-16 17:05:56 +0100

    renaming

---
+++ app/src/main/java/com/locator_app/locator/LoginRegisterStart.java
@@ -0,0 +1,14 @@
+package com.locator_app.locator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginRegisterStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_register_start);
+        //TODO: getDeviceId
+    }
+}
