ab05a3d501a4e545051182dc2ded045a110916a6
Dennis Moosmann <dennis.moosmann@gmx.de>
2015-12-18 15:59:50 +0100

    set title of action bar

--- app/src/main/java/com/locator_app/locator/view/LoginRegisterActivity.java
+++ app/src/main/java/com/locator_app/locator/view/LoginRegisterActivity.java
@@ -1,11 +1,13 @@
 package com.locator_app.locator.view;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.locator_app.locator.R;
@@ -29,10 +31,19 @@ public class LoginRegisterActivity extends AppCompatActivity {
         login = (ImageView) findViewById(R.id.login);
         loginFacebook = (ImageView) findViewById(R.id.login_facebook);
         register = (ImageView) findViewById(R.id.register);
+
         loadBitmap(R.drawable.locator_logo, locatorLogo, 500, 300);
         loadBitmap(R.drawable.login, login, 200, 170);
         loadBitmap(R.drawable.login_facebook, loginFacebook, 200, 170);
         loadBitmap(R.drawable.register, register, 200, 170);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), LoginMailActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     //Load every image in separate thread
