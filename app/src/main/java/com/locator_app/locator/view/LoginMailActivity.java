505c47dd46eae8d29b6322733a0389e2fb754cb0
Dennis Moosmann <dennis.moosmann@gmx.de>
2015-12-19 09:39:48 +0100

    add onkeyListener

--- app/src/main/java/com/locator_app/locator/view/LoginMailActivity.java
+++ app/src/main/java/com/locator_app/locator/view/LoginMailActivity.java
@@ -1,10 +1,14 @@
 package com.locator_app.locator.view;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.locator_app.locator.R;
@@ -12,6 +16,7 @@ import com.locator_app.locator.R;
 public class LoginMailActivity extends AppCompatActivity {
 
     private TextView actionbarTitle;
+    private EditText loginMail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +24,23 @@ public class LoginMailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login_mail);
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.actionbar_custom);
-        actionbarTitle = (TextView) findViewById(R.id.actionbar_title);
-        actionbarTitle.setText(R.string.login);
+//        actionbarTitle = (TextView) findViewById(R.id.actionbar_title);
+//        actionbarTitle.setText(R.string.login);
+        View view = (View) findViewById(R.id.actionbar_title);
+        loginMail = (EditText) findViewById(R.id.loginMail);
+
+        loginMail.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                    Intent intent = new Intent(v.getContext(), LoginRegisterActivity.class);
+                    startActivity(intent);
+                    return true;
+                }
+                return false;
+            }
+        });
     }
+
+
 }
