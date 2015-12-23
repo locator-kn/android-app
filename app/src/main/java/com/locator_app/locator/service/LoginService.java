b0b0629b6e430091318b51e14430597c887d4c69
Dennis Moosmann <dennis.moosmann@gmx.de>
2015-12-23 21:04:46 +0100

    add field annotation

--- app/src/main/java/com/locator_app/locator/service/LoginService.java
+++ app/src/main/java/com/locator_app/locator/service/LoginService.java
@@ -3,15 +3,16 @@ package com.locator_app.locator.service;
 import com.locator_app.locator.model.Login;
 import com.locator_app.locator.util.Server;
 
-import org.androidannotations.annotations.rest.Get;
 import org.androidannotations.annotations.rest.Post;
 import org.androidannotations.annotations.rest.Rest;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
-import java.util.List;
+import retrofit.Call;
+import retrofit.http.Body;
+import retrofit.http.Field;
 
 @Rest(rootUrl = Server.apiUrl, converters = MappingJackson2HttpMessageConverter.class)
 public interface LoginService {
-    @Post("/login")
-    Login login();
+    @Post("/users/login")
+    Call<Login> login(@Field("mail") String mail, @Field("password") String password);
 }
