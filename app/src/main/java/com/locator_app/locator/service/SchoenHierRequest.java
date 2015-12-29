364b9127d89a8d0c2b742b02489cad6536f22863
Markus Heilig <markusheilig@gmx.net>
2015-12-29 16:24:04 +0100

    created SchoenHierRequest

---
+++ app/src/main/java/com/locator_app/locator/service/SchoenHierRequest.java
@@ -0,0 +1,13 @@
+package com.locator_app.locator.service;
+
+import com.google.gson.annotations.SerializedName;
+
+public class SchoenHierRequest {
+
+    @SerializedName("long")
+    public double lon;
+
+    @SerializedName("lat")
+    public double lat;
+
+}
