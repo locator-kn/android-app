9649c28aa8ef0fa91181451156a1bdc1a2078f91
Dennis Moosmann <dennis.moosmann@gmx.de>
2015-12-30 13:44:32 +0100

    register mail

---
+++ app/src/main/java/com/locator_app/locator/view/RegisterMailActivity.java
@@ -0,0 +1,68 @@
+
+package com.locator_app.locator.view;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.locator_app.locator.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class RegisterMailActivity extends AppCompatActivity {
+
+    @Bind(R.id.registerMail)
+    EditText registerMail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_mail);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.actionbar_custom);
+        ButterKnife.bind(this);
+
+        //set action bar title and color
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable
+                (ContextCompat.getColor(getApplicationContext(), R.color.colorRegister)));
+        View v = getSupportActionBar().getCustomView();
+        TextView titleTxtView = (TextView) v.findViewById(R.id.actionbar_title);
+        titleTxtView.setText(R.string.register);
+
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+
+        registerMail.setOnKeyListener((v1, keyCode, event) -> {
+            if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                String mail = registerMail.getText().toString();
+                if (!isValidEmail(mail)) {
+                    Toast.makeText(getApplicationContext(),
+                            "E-Mail nicht vollständig oder fehlerhaft!",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent(v1.getContext(), LoginPasswordActivity.class);
+                    intent.putExtra("mail", mail);
+                    startActivity(intent);
+                    return true;
+                }
+            }
+            return false;
+        });
+    }
+
+    public boolean isValidEmail(String email) {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+}
