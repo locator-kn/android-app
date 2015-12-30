a3a42bb03d1f3cbc5246e53def69a771f6e9a606
Markus Heilig <markusheilig@gmx.net>
2015-12-30 08:17:28 +0100

    logout button on homescreen (for test reasons)

--- app/src/main/java/com/locator_app/locator/view/HomeActivity.java
+++ app/src/main/java/com/locator_app/locator/view/HomeActivity.java
@@ -1,12 +1,16 @@
 package com.locator_app.locator.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.locator_app.locator.R;
+import com.locator_app.locator.model.SchoenHier;
+import com.locator_app.locator.service.LoginResponse;
 import com.locator_app.locator.service.LogoutResponse;
+import com.locator_app.locator.service.RegistrationRequest;
 import com.locator_app.locator.service.SchoenHierRequestManager;
 import com.locator_app.locator.service.UsersRequestManager;
 
@@ -31,17 +35,21 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     @OnClick(R.id.button)
-    public void register() {
-        UsersRequestManager manager = new UsersRequestManager();
-        manager.logout()
+    public void logout() {
+
+        UsersRequestManager requestManager = new UsersRequestManager();
+        requestManager.logout()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
-                        (logoutResponse) -> {
-                            Toast.makeText(getApplicationContext(), logoutResponse.message, Toast.LENGTH_SHORT).show();
+                        (val) -> {
+                            Toast.makeText(getApplicationContext(), "logged out", Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(getApplicationContext(), LoginRegisterStartActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
                         },
-                        (error) -> {
-                            Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                        (err) -> {
+                            Toast.makeText(getApplicationContext(), "could not log out, sorry!", Toast.LENGTH_LONG).show();
                         }
                 );
     }
