95bd4d5037bc49ac7d6ac2ca5dcc3a57fa0d9c50
Markus Heilig <markusheilig@gmx.net>
2015-12-30 08:13:57 +0100

    created LocatorApplication.java for global context access

---
+++ app/src/main/java/com/locator_app/locator/LocatorApplication.java
@@ -0,0 +1,19 @@
+package com.locator_app.locator;
+
+
+import android.app.Application;
+import android.content.Context;
+
+public class LocatorApplication extends Application {
+
+    private static Context context;
+
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+    }
+
+    public static Context getAppContext() {
+        return context;
+    }
+}
