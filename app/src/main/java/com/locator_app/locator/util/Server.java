7d70079e80754060e692260b866983f7d426bbb3
Markus Heilig <markusheilig@gmx.net>
2015-12-30 10:17:20 +0100

    unused class

--- app/src/main/java/com/locator_app/locator/util/Server.java
+++
@@ -1,6 +0,0 @@
-package com.locator_app.locator.util;
-
-public class Server {
-    public static final String url = "https://locator-app.com";
-    public static final String apiUrl = url + "/api/v1";
-}
