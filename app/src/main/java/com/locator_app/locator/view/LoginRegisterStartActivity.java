0d3a7005164089c77ab60150de1772511b504222
Markus Heilig <markusheilig@gmx.net>
2015-12-30 13:33:14 +0100

    disabled back-navigation

--- app/src/main/java/com/locator_app/locator/view/LoginRegisterStartActivity.java
+++ app/src/main/java/com/locator_app/locator/view/LoginRegisterStartActivity.java
@@ -70,6 +70,7 @@ public class LoginRegisterStartActivity extends AppCompatActivity {
 
     private void jumpToHomeScreen() {
         Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
 
