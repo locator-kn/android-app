37e8de033f3dd8407f10337e0d871719ee956456
Markus Heilig <markusheilig@gmx.net>
2016-01-03 15:38:13 +0100

    combined schoenhier api with service

--- app/src/main/java/com/locator_app/locator/service/schoenhier/SchoenHierApiService.java
+++ app/src/main/java/com/locator_app/locator/service/schoenhier/SchoenHierApiService.java
@@ -1,14 +1,32 @@
 package com.locator_app.locator.service.schoenhier;
 
+import com.locator_app.locator.service.Api;
 import com.locator_app.locator.service.ServiceFactory;
 
 import java.net.UnknownHostException;
 
 import retrofit.HttpException;
 import retrofit.Response;
+import retrofit.http.Body;
+import retrofit.http.GET;
+import retrofit.http.POST;
+import retrofit.http.Query;
 import rx.Observable;
 
 public class SchoenHierApiService {
+
+
+    public interface SchoenHierApi {
+
+        @GET(Api.version + "/schoenhiers/nearby")
+        Observable<Response> schoenHiersNearby(@Query("long") double lon,
+                                               @Query("lat") double lat,
+                                               @Query("maxDistance") double distance,
+                                               @Query("limit") int limit);
+
+        @POST(Api.version + "/schoenhiers")
+        Observable<Response> markAsSchoenHier(@Body SchoenHierRequest schoenHierRequest);
+    }
 
     private SchoenHierApi service = ServiceFactory.createService(SchoenHierApi.class);
 
