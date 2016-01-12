0c19da7a227e3b9969c21dc0a27a9745d55b2b63
Markus Heilig <markusheilig@gmx.net>
2016-01-12 09:07:59 +0100

    image storing

--- app/src/main/java/com/locator_app/locator/db/Couch.java
+++ app/src/main/java/com/locator_app/locator/db/Couch.java
@@ -1,16 +1,30 @@
 package com.locator_app.locator.db;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+
+import com.couchbase.lite.Attachment;
 import com.couchbase.lite.CouchbaseLiteException;
 import com.couchbase.lite.Database;
 import com.couchbase.lite.Document;
 import com.couchbase.lite.Manager;
+import com.couchbase.lite.Revision;
+import com.couchbase.lite.SavedRevision;
+import com.couchbase.lite.UnsavedRevision;
 import com.couchbase.lite.android.AndroidContext;
 import com.locator_app.locator.LocatorApplication;
 import com.locator_app.locator.model.User;
+import com.locator_app.locator.util.BitmapHelper;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import rx.Observable;
 
@@ -85,6 +99,61 @@ public class Couch {
             e.printStackTrace();
         }
         switchToDefaultDatabase();
+    }
+
+    private boolean isLocalImage(String imageUri) {
+        return imageUri.startsWith("drawable://");
+    }
+
+    public void deleteAllImages() {
+        Document doc = database.getDocument("images");
+        if (doc != null) {
+            SavedRevision savedRevision = doc.getCurrentRevision();
+            if (savedRevision != null) {
+                UnsavedRevision unsavedRevision = savedRevision.createRevision();
+                for (String attachment: savedRevision.getAttachmentNames()) {
+                    unsavedRevision.removeAttachment(attachment);
+                    Log.d("Couch", "removed attachment " + attachment);
+                }
+            }
+        }
+    }
+
+    public void storeImage(String imageUri, Bitmap image) {
+        if (isLocalImage(imageUri))
+            return;
+        Document task = database.getDocument("images");
+        UnsavedRevision newRev = task.getCurrentRevision().createRevision();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.JPEG, 100, out);
+        ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
+        newRev.setAttachment(imageUri, "image/jpeg", in);
+        try {
+            newRev.save();
+            Log.d("Couch", "stored image to database: " + imageUri);
+        } catch (CouchbaseLiteException e) {
+            Log.d("Couch", "could not store image: " + imageUri);
+        }
+    }
+
+    public Bitmap image(String imageUri) {
+        if (isLocalImage(imageUri))
+            return null;
+        Document task = database.getDocument("images");
+        Revision rev = task.getCurrentRevision();
+        Attachment att = rev.getAttachment(imageUri);
+        if (att == null) {
+            Log.d("Couch", "found no attachment for image " + imageUri);
+            return null;
+        }
+        try {
+            Bitmap bitmap = BitmapFactory.decodeStream(att.getContent());
+            Log.d("Couch", "loaded image from couch " + imageUri);
+            return bitmap;
+        } catch (CouchbaseLiteException e) {
+            Log.d("Couch", "could not decode input stream from Attachment.getContent()");
+        }
+        return null;
     }
 
     private <T> T get(Map<String, Object> map, String key, T defaultValue) {
