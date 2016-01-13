4acd6124f8b4bbe5421f13c3f1b78a4a939d7723
niwonneb <niwonneb@htwg-konstanz.de>
2016-01-13 20:59:36 +0100

    Added Current Position Image

--- app/src/main/java/com/locator_app/locator/view/MapsActivity.java
+++ app/src/main/java/com/locator_app/locator/view/MapsActivity.java
@@ -43,7 +43,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         String urlCurrentPos = "drawable://" + R.drawable.profile;
         CacheImageLoader.getInstance().loadAsync(urlCurrentPos).subscribe(
-                (bitmap -> currentPos = bitmap),
+                (bitmap -> {
+                    currentPos = Bitmap.createScaledBitmap(bitmap, 60, 60, false);
+                }),
                 (error -> {})
         );
 
@@ -66,10 +68,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         LatLng locationPos = new LatLng(location.getLatitude(), location.getLongitude());
 
-//        BitmapDescriptor currentPosDesc = BitmapDescriptorFactory.fromBitmap(currentPos);
-//        googleMap.addMarker(new MarkerOptions().position(locationPos)
-//                .icon(currentPosDesc)
-//                .anchor((float) 0.5, (float) 0.5));
+        BitmapDescriptor currentPosDesc = BitmapDescriptorFactory.fromBitmap(currentPos);
+        googleMap.addMarker(new MarkerOptions().position(locationPos)
+                .icon(currentPosDesc)
+                .anchor((float) 0.5, (float) 0.5));
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(locationPos, 15));
         addHeatMap(location.getLongitude(), location.getLatitude());
     }
