7ca6eb7e176eaf2e53ee7a240124cc2368d22df6
Markus Heilig <markusheilig@gmx.net>
2016-01-13 16:04:27 +0100

    added layout reference

--- app/src/main/java/com/locator_app/locator/view/ProfileActivity.java
+++ app/src/main/java/com/locator_app/locator/view/ProfileActivity.java
@@ -3,6 +3,8 @@ package com.locator_app.locator.view;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.locator_app.locator.R;
+
 public class ProfileActivity extends Activity {
 
     @Override
