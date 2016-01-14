61db320067c7076792e996e931e6adb2adb59ff9
Markus Heilig <markusheilig@gmx.net>
2016-01-14 13:17:32 +0100

    implemented RxBus

--- app/src/main/java/com/locator_app/locator/util/RxBus.java
+++ app/src/main/java/com/locator_app/locator/util/RxBus.java
@@ -1,6 +1,29 @@
 package com.locator_app.locator.util;
 
 
+
+import rx.Observable;
+import rx.subjects.PublishSubject;
+import rx.subjects.SerializedSubject;
+import rx.subjects.Subject;
+
 public class RxBus {
-    
+
+    private final Subject<Object, Object> bus = new SerializedSubject<>(PublishSubject.create());
+
+    public void send(Object event) {
+        bus.onNext(event);
+    }
+
+    public Observable<Object> toObservable() {
+        return bus;
+    }
+
+    private static RxBus globalBusInstance;
+    synchronized public static RxBus globalBus() {
+        if (globalBusInstance == null) {
+            globalBusInstance = new RxBus();
+        }
+        return globalBusInstance;
+    }
 }
