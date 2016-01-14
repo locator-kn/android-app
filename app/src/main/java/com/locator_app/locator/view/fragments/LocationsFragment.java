f4bea1b21176dd21ec6dce169e6b8ade57a8fdd6
Markus Heilig <markusheilig@gmx.net>
2016-01-14 16:54:58 +0100

    location loading

--- app/src/main/java/com/locator_app/locator/view/fragments/LocationsFragment.java
+++ app/src/main/java/com/locator_app/locator/view/fragments/LocationsFragment.java
@@ -7,11 +7,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.locator_app.locator.LocatorApplication;
+import com.locator_app.locator.controller.LocationController;
+import com.locator_app.locator.model.LocatorLocation;
 import com.locator_app.locator.view.adapter.RowLayoutAdapter;
 
-import rx.Observable;
+import java.util.LinkedList;
+import java.util.List;
+
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class LocationsFragment extends Fragment {
 
@@ -28,6 +35,24 @@ public class LocationsFragment extends Fragment {
     }
 
     private void loadAdapterItems() {
+        List<RowLayoutAdapter.RowLayoutItem> rowLayoutItems = new LinkedList<>();
+        LocationController.getInstance().getLocationsNearby(9.169753789901733, 47.66868204997508, 2, 20)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .map(this::locationToRowLayoutItem)
+                .subscribe(
+                        (item) -> rowLayoutItems.add(item),
+                        (error) -> Toast.makeText(LocatorApplication.getAppContext(),
+                                "something went wrong :-(", Toast.LENGTH_SHORT).show(),
+                        ()->adapter.setRowLayoutItems(rowLayoutItems)
+                );
+    }
 
+    private RowLayoutAdapter.RowLayoutItem locationToRowLayoutItem(LocatorLocation location) {
+        RowLayoutAdapter.RowLayoutItem item = new RowLayoutAdapter.RowLayoutItem();
+        item.imageUri = location.thumbnailUri();
+        item.text = location.title;
+        item.description = location.description;
+        return item;
     }
 }
