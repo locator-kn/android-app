326105ba33e726e847c544f59557d9f8aeaebb2d
Markus Heilig <markusheilig@gmx.net>
2016-01-15 11:45:09 +0100

    added user attributes

--- app/src/main/java/com/locator_app/locator/model/User.java
+++ app/src/main/java/com/locator_app/locator/model/User.java
@@ -1,11 +1,46 @@
 package com.locator_app.locator.model;
 
+import com.google.gson.annotations.SerializedName;
+import com.locator_app.locator.R;
+
 import java.io.Serializable;
 
-public class User implements Serializable {
-    public String mail = "";
-    public String residence = "";
-    public String name = "";
+public class User implements LocatorObject, Serializable {
+
+    @SerializedName("_id")
     public String _id = "";
+
+    @SerializedName("birthdate")
+    public String birthdate = "";
+
+    @SerializedName("residence")
+    public String residence = "";
+
+    @SerializedName("description")
+    public String description = "";
+
+    @SerializedName("mail")
+    public String mail = "";
+
+    @SerializedName("surname")
+    public String surname = "";
+
+    @SerializedName("name")
+    public String name = "";
+
+    @SerializedName("picture")
+    public String picture = "";
+
+    @SerializedName("thumb")
+    public String thumb = "";
+
     public boolean loggedIn = false;
+
+    @Override
+    public String thumbnailUri() {
+        if (thumb.isEmpty()) {
+            return "drawable://" + R.drawable.profile;
+        }
+        return thumb;
+    }
 }
