efcde4aa9a18c57a42bc56c3c1fb095f7095e687
Markus Heilig <markusheilig@gmx.net>
2016-01-17 22:09:39 +0100

    implemented UserRecyclerViewAdapter

--- app/src/main/java/com/locator_app/locator/view/adapter/UserRecyclerViewAdapter.java
+++ app/src/main/java/com/locator_app/locator/view/adapter/UserRecyclerViewAdapter.java
@@ -2,14 +2,20 @@ package com.locator_app.locator.view.adapter;
 
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.locator_app.locator.R;
 import com.locator_app.locator.model.User;
+import com.locator_app.locator.view.bubble.BubbleView;
 
 import java.util.LinkedList;
 import java.util.List;
 
-public class UserRecyclerViewAdapter extends RecyclerView.Adapter<LocationRecyclerViewAdapter.ViewHolder> {
+public class UserRecyclerViewAdapter extends RecyclerView.Adapter<UserRecyclerViewAdapter.ViewHolder> {
 
     List<User> users = new LinkedList<>();
 
@@ -19,17 +25,47 @@ public class UserRecyclerViewAdapter extends RecyclerView.Adapter<LocationRecycl
     }
 
     @Override
-    public LocationRecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public UserRecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.default_list_item, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(LocationRecyclerViewAdapter.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(UserRecyclerViewAdapter.ViewHolder holder, int position) {
+        final User user = users.get(position);
+        holder.update(user);
+        holder.view.setOnClickListener(v ->
+                        Toast.makeText(v.getContext(), user.surname, Toast.LENGTH_SHORT).show()
+        );
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return users.size();
+    }
+
+    static class ViewHolder extends LocationRecyclerViewAdapter.ViewHolder {
+        public final View view;
+        public final TextView name;
+        public final TextView description;
+        public final TextView bubbleInfo;
+        public final BubbleView bubbleView;
+
+        public ViewHolder(View view) {
+            super(view);
+            this.view = view;
+            name = (TextView) view.findViewById(R.id.text);
+            description = (TextView) view.findViewById(R.id.description);
+            bubbleInfo = (TextView) view.findViewById(R.id.bubble_info);
+            bubbleView = (BubbleView) view.findViewById(R.id.bubbleView);
+        }
+
+        public void update(User user) {
+            title.setText(user.name);
+            description.setText(user.description);
+            bubbleView.loadImage(user.thumbnailUri());
+            bubbleInfo.setText("");
+        }
     }
 }
