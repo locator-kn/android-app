14ac49345a500e9d32f6f1d0b157b9e1a3b64575
Markus Heilig <markusheilig@gmx.net>
2016-01-22 21:44:13 +0100

    install persistent cookie store

--- app/src/main/java/com/locator_app/locator/apiservice/ServiceFactory.java
+++ app/src/main/java/com/locator_app/locator/apiservice/ServiceFactory.java
@@ -1,7 +1,13 @@
 package com.locator_app.locator.apiservice;
 
+import com.locator_app.locator.LocatorApplication;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.logging.HttpLoggingInterceptor;
+
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.CookiePolicy;
+import java.net.CookieStore;
 
 import retrofit.GsonConverterFactory;
 import retrofit.Retrofit;
@@ -18,6 +24,10 @@ public class ServiceFactory {
             HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
             interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
             client.interceptors().add(interceptor);
+
+            CookieStore cookieStore = new PersistentCookieStore();
+            CookieManager cookieManager = new CookieManager(cookieStore, CookiePolicy.ACCEPT_ALL);
+            CookieHandler.setDefault(cookieManager);
 
             retrofit = new Retrofit.Builder()
                     .baseUrl("https://locator-app.com")
