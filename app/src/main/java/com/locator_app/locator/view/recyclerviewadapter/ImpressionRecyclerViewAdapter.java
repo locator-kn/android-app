2cc9e7fc17cb004e76324b99c630a9eed0d36ee1
Markus Heilig <markusheilig@gmx.net>
2016-01-30 09:07:44 +0100

    integrated new cards

--- app/src/main/java/com/locator_app/locator/view/recyclerviewadapter/ImpressionRecyclerViewAdapter.java
+++ app/src/main/java/com/locator_app/locator/view/recyclerviewadapter/ImpressionRecyclerViewAdapter.java
@@ -7,13 +7,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.locator_app.locator.R;
-import com.locator_app.locator.apiservice.Api;
 import com.locator_app.locator.controller.UserController;
+import com.locator_app.locator.model.LocatorLocation;
 import com.locator_app.locator.model.impressions.AbstractImpression;
 import com.locator_app.locator.model.impressions.AbstractImpression.ImpressionType;
 import com.locator_app.locator.model.impressions.ImageImpression;
@@ -25,52 +26,75 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-import jp.wasabeef.glide.transformations.CropCircleTransformation;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
 public class ImpressionRecyclerViewAdapter
         extends RecyclerView.Adapter<ImpressionRecyclerViewAdapter.ViewHolder>{
 
+    final int locationDescriptionViewType = 100;
+    final int createNewImpressionViewType = 200;
+
     List<AbstractImpression> impressions = new LinkedList<>();
+    LocatorLocation location = null;
     final List<AbstractImpression.ImpressionType> supportedImpressionTypes =
             Arrays.asList(ImpressionType.IMAGE, ImpressionType.VIDEO, ImpressionType.TEXT);
 
-    public void setImpressions(List<AbstractImpression> impressions) {
+    public void setImpressions(List<AbstractImpression> impressions, LocatorLocation location) {
+        this.location = location;
         this.impressions = impressions;
         notifyDataSetChanged();
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        ImpressionType type = supportedImpressionTypes.get(viewType);
-        if (type == ImpressionType.IMAGE) {
-            final int cardId = R.layout.card_impression_image;
+        if (viewType == createNewImpressionViewType) {
+            final int cardId = R.layout.card_new_impression;
             View v = LayoutInflater.from(parent.getContext()).inflate(cardId, parent, false);
-            return new ImageImpressionViewHolder(v);
-        } else if (type == ImpressionType.TEXT) {
-            final int cardId = R.layout.card_impression_text;
+            return new CreateImpressionViewHolder(v);
+        } else if (viewType == locationDescriptionViewType) {
+            final int cardId = R.layout.card_location_description;
             View v = LayoutInflater.from(parent.getContext()).inflate(cardId, parent, false);
-            return new TextImpressionViewHolder(v);
-        } else if (type == ImpressionType.VIDEO) {
+            return new LocationDescriptionViewHolder(v);
+        } else {
+            ImpressionType type = supportedImpressionTypes.get(viewType);
+            if (type == ImpressionType.IMAGE) {
+                final int cardId = R.layout.card_impression_image;
+                View v = LayoutInflater.from(parent.getContext()).inflate(cardId, parent, false);
+                return new ImageImpressionViewHolder(v);
+            } else if (type == ImpressionType.TEXT) {
+                final int cardId = R.layout.card_impression_text;
+                View v = LayoutInflater.from(parent.getContext()).inflate(cardId, parent, false);
+                return new TextImpressionViewHolder(v);
+            } else if (type == ImpressionType.VIDEO) {
 
+            }
         }
         return null;
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.bind(impressions.get(position));
+        if (position >= 2) {
+            holder.bind(impressions.get(position - 2));
+        }
     }
 
     @Override
     public int getItemCount() {
-        return impressions.size();
+        if (impressions.isEmpty())
+            return 0;
+        return impressions.size() + 2;
     }
 
     @Override
     public int getItemViewType(int position) {
-        ImpressionType type = impressions.get(position).type();
+        if (position == 0) { // show location description card
+            return locationDescriptionViewType;
+        } else if (position == 1) { // create new impression card
+            return createNewImpressionViewType;
+        }
+        ImpressionType type = impressions.get(position - 2).type();
         return supportedImpressionTypes.indexOf(type);
     }
 
@@ -169,6 +193,60 @@ public class ImpressionRecyclerViewAdapter
                     );
             date.setText(DateConverter.toddMMyyyy(textImpression.getCreateDate()));
             impressionText.setText(textImpression.getText());
+        }
+    }
+
+    class LocationDescriptionViewHolder extends ViewHolder {
+
+        public LocationDescriptionViewHolder(View itemView) {
+            super(itemView);
+            TextView description = (TextView)itemView.findViewById(R.id.description);
+            description.setText(location.description);
+            description.setVisibility(View.GONE);
+            itemView.setOnClickListener(v -> {
+                if (description.getVisibility() == View.GONE) {
+                    description.setVisibility(View.VISIBLE);
+                } else {
+                    description.setVisibility(View.GONE);
+                }
+            });
+        }
+
+        @Override
+        public void bind(AbstractImpression impression) {
+        }
+    }
+
+    class CreateImpressionViewHolder extends ViewHolder {
+
+        public CreateImpressionViewHolder(View itemView) {
+            super(itemView);
+            TextView numberOfImpressions = (TextView)itemView.findViewById(R.id.numberOfImpressions);
+            numberOfImpressions.setText(Integer.toString(impressions.size()));
+            LinearLayout impressionTypes = (LinearLayout)itemView.findViewById(R.id.impressionTypes);
+            impressionTypes.setVisibility(View.GONE);
+            LinearLayout showHideImpressionTypes = (LinearLayout)itemView.findViewById(R.id.showHideImpressionTypes);
+            showHideImpressionTypes.setOnClickListener(v -> {
+                if (impressionTypes.getVisibility() == View.GONE) {
+                    impressionTypes.setVisibility(View.VISIBLE);
+                } else {
+                    impressionTypes.setVisibility(View.GONE);
+                }
+            });
+            ImageView voice = (ImageView)itemView.findViewById(R.id.voiceImpression);
+            voice.setOnClickListener(v -> Toast.makeText(itemView.getContext(), "voice", Toast.LENGTH_SHORT).show());
+            ImageView media = (ImageView)itemView.findViewById(R.id.mediaImpression);
+            media.setOnClickListener(v -> Toast.makeText(itemView.getContext(), "photo", Toast.LENGTH_SHORT).show());
+            media.setOnLongClickListener(v -> {
+                Toast.makeText(itemView.getContext(), "video", Toast.LENGTH_SHORT).show();
+                return true;
+            });
+            ImageView text = (ImageView)itemView.findViewById(R.id.textImpression);
+            text.setOnClickListener(v -> Toast.makeText(itemView.getContext(), "text", Toast.LENGTH_SHORT).show());
+        }
+
+        @Override
+        public void bind(AbstractImpression impression) {
         }
     }
 }
