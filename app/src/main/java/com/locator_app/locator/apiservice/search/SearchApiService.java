6de001300507eaebd7ed43483afb44134d59989d
Markus Heilig <markusheilig@gmx.net>
2016-02-26 15:19:02 +0100

    use GenericErrorHandler

--- app/src/main/java/com/locator_app/locator/apiservice/search/SearchApiService.java
+++ app/src/main/java/com/locator_app/locator/apiservice/search/SearchApiService.java
@@ -3,14 +3,10 @@ package com.locator_app.locator.apiservice.search;
 import com.locator_app.locator.apiservice.Api;
 import com.locator_app.locator.apiservice.ServiceFactory;
 import com.locator_app.locator.apiservice.errorhandling.GenericErrorHandler;
-import com.locator_app.locator.apiservice.schoenhier.SchoenHiersNearbyResponse;
-import com.locator_app.locator.apiservice.users.RegistrationResponse;
 import com.locator_app.locator.model.LocatorLocation;
 
-import java.net.UnknownHostException;
 import java.util.List;
 
-import retrofit.HttpException;
 import retrofit.Response;
 import retrofit.http.GET;
 import retrofit.http.Path;
