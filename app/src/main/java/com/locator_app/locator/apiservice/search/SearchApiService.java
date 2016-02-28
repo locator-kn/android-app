8286fd48516ce9d46e649a74441aadc926a7281c
niwonneb <niwonneb@htwg-konstanz.de>
2016-02-28 16:37:32 +0100

    fixed search result parsing

--- app/src/main/java/com/locator_app/locator/apiservice/search/SearchApiService.java
+++ app/src/main/java/com/locator_app/locator/apiservice/search/SearchApiService.java
@@ -3,9 +3,11 @@ package com.locator_app.locator.apiservice.search;
 import com.locator_app.locator.apiservice.Api;
 import com.locator_app.locator.apiservice.ServiceFactory;
 import com.locator_app.locator.apiservice.errorhandling.GenericErrorHandler;
+import com.locator_app.locator.apiservice.locations.LocationsNearbyResponse;
 import com.locator_app.locator.model.LocatorLocation;
 
 import java.util.List;
+import java.util.Vector;
 
 import retrofit.Response;
 import retrofit.http.GET;
@@ -41,7 +43,10 @@ public class SearchApiService {
     }
 
     private Observable<List<LocatorLocation>> parseResponseToList(SearchResponse response) {
-        List<LocatorLocation> allLocations = response.locatorLocations;
+        List<LocatorLocation> allLocations = new Vector<>();
+        for (LocationsNearbyResponse.Result locationResult : response.locatorLocations) {
+            allLocations.add(locationResult.location);
+        }
         allLocations.addAll(response.googleLocations);
         return Observable.just(allLocations);
     }
