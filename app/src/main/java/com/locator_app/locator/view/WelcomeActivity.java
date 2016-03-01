ce328e74c1c494c35084b376f16cef26c451d035
Markus Heilig <markusheilig@gmx.net>
2016-03-01 15:39:51 +0100

    created WelcomeActivity

---
+++ app/src/main/java/com/locator_app/locator/view/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package com.locator_app.locator.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.locator_app.locator.R;
+
+public class WelcomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
