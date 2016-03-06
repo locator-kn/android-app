6f788b171e27f3af4ca6a355dc04b8108c666031
Markus Heilig <markusheilig@gmx.net>
2016-03-06 18:56:32 +0100

    changed tablayout color

--- app/src/main/java/com/locator_app/locator/view/profile/ProfileActivity.java
+++ app/src/main/java/com/locator_app/locator/view/profile/ProfileActivity.java
@@ -2,6 +2,7 @@ package com.locator_app.locator.view.profile;
 
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentActivity;
@@ -41,7 +42,6 @@ public class ProfileActivity extends FragmentActivity {
     TextView userName;
 
     @Bind(R.id.profileImageView)
-    //CircleImageView profileImageBubbleView;
     ImageView profileImageBubbleView;
 
     @Bind(R.id.tabLayout)
@@ -70,6 +70,8 @@ public class ProfileActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         ButterKnife.bind(this);
+
+        tabLayout.setSelectedTabIndicatorColor(Color.parseColor("#FFFFFF"));
 
         user = (User) getIntent().getSerializableExtra("profile");
 
